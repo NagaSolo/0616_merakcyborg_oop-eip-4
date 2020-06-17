5d3dbf626a37b2328067294c51efe233d389b97c
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-06-17 08:52:00 +0100

    update option

--- tempahan.py
+++ tempahan.py
@@ -1,4 +1,20 @@
+
 print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
 print('.............. Sistem Tempahan .................\n')
 print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
 
+print('Sebagai Admin: Tekan \'A\'')
+print('\n')
+print('Sebagai Pengguna: Tekan \'U\'')
+print('\n')
+print('Keluar dari program: Tekan \'Q\'')
+print('\n')
+pilihan = input()
+if (pilihan == 'A' or 'a'):
+    print('Under construction - Sebagai Admin')
+elif (pilihan == 'U' or 'u'):
+    print('Under construction - Sebagai User')
+elif (pilihan == 'Q' or 'q'):
+    exit()
+else:
+    print('Pilihan tidak sah')
