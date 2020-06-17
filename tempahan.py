4e3f1fe5e1258c02d1654ce1273461536bfe18fa
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-06-17 09:05:27 +0100

    program looping to sustain state session

--- tempahan.py
+++ tempahan.py
@@ -1,20 +1,23 @@
 
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
-print('.............. Sistem Tempahan .................\n')
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+program_masih_berjalan = True
 
-print('Sebagai Admin: Tekan \'A\'')
-print('\n')
-print('Sebagai Pengguna: Tekan \'U\'')
-print('\n')
-print('Keluar dari program: Tekan \'Q\'')
-print('\n')
-pilihan = input()
-if (pilihan == 'A' or 'a'):
-    print('Under construction - Sebagai Admin')
-elif (pilihan == 'U' or 'u'):
-    print('Under construction - Sebagai User')
-elif (pilihan == 'Q' or 'q'):
-    exit()
-else:
-    print('Pilihan tidak sah')
+while program_masih_berjalan:
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+    print('.............. Sistem Tempahan .................\n')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+
+    print('Sebagai Admin: Tekan \'A\'')
+    print('\n')
+    print('Sebagai Pengguna: Tekan \'U\'')
+    print('\n')
+    print('Keluar dari program: Tekan \'Q\'')
+    print('\n')
+    pilihan = input()
+    if pilihan == 'A':
+        print('Under construction - Sebagai Admin\n')
+    elif pilihan == 'U':
+        print ('Under construction - Sebagai User\n')
+    elif pilihan == 'Q':
+        program_masih_berjalan = False
+    else:
+        print('Pilihan tidak sah')
